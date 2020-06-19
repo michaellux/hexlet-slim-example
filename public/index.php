dfaa116bb29ddda651c6708694573734b73d1258
michaellux <michaellux@yandex.ua>
2020-06-19 11:25:19 +0300

    change slim settings by using DI/Container for connecting templates

--- public/index.php
+++ public/index.php
@@ -4,8 +4,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
